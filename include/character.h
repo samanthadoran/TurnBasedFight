2b64928a14509a41d7e03753bc7b5f1b061bf6f9
Samantha Doran <Vixen.Miyuki@gmail.com>
2013-07-30 02:55:06 -0400

    Add a vector of actions to Character, an actionSelect menu, a getTarget function, and a listActions function.

--- include/character.h
+++ include/character.h
@@ -2,6 +2,8 @@
 #define CHARACTER_H
 
 #include <string>
+#include <vector>
+#include <iostream>
 #include "action.h"
 
 class Action;
@@ -15,12 +17,17 @@ class Character
         double getLife() const;
         std::string getName() const;
         std::string toString() const;
+        std::string listActions() const;
         void setTarget(Character *);
+        Character * getTarget();
+        Action * selectAction();
+        void addAction(Action*);
         std::string doAction() const;
         std::string doAction(Action *);
         void setLife(double);
     protected:
     private:
+        std::vector<Action*> actions;
         Character * target;
         std::string name;
         double life;
--- src/character.cpp
+++ src/character.cpp
@@ -11,6 +11,30 @@ Character::Character(std::string name, double life)
     this->life = life;
 }
 
+Action * Character::selectAction()
+{
+    //Prompt the user for their choice
+    int selection = 0;
+    do
+    {
+        std::cout << toString() << std::endl;
+        std::cout << "-1. Return to the previous menu..." << std::endl;
+        std::cout << listActions() << std::endl;
+        std::cin >> selection;
+        std::cout << std::endl;
+    }while(selection == 0 || selection > actions.size());
+
+    if(selection == -1)
+    {
+        std::cout << "Returning to main menu." << std::endl;
+        return nullptr;
+    }
+    else
+    {
+        return actions[selection-1];
+    }
+}
+
 double Character::getLife() const
 {
     return life;
@@ -21,20 +45,39 @@ std::string Character::getName() const
     return name;
 }
 
+std::string Character::listActions() const
+{
+    //std::string toRet = toString();
+    std::stringstream toRet;
+    //toRet += "\n";
+    for(int i = 0; i < actions.size(); ++i)
+    {
+        toRet << " " << i + 1 << ". " << actions[i]->toString() << "\n";
+    }
+    //return toRet.append(working.str());
+    return toRet.str();
+}
+
 std::string Character::toString() const
 {
     std::stringstream toRet;
     toRet << name << ": " << life;
-    if(target == NULL)
+    if(target == nullptr)
         toRet << "\t No current target!";
     else
         toRet << "\t Current target: " << target->getName() << ".";
+
     return toRet.str();
 }
 
 void Character::setTarget(Character * target)
 {
     this->target = target;
+}
+
+Character * Character::getTarget()
+{
+    return target;
 }
 
 std::string Character::doAction() const
@@ -48,6 +91,11 @@ std::string Character::doAction(Action * act)
 {
     std::stringstream toRet;
     return act->use(this, target);
+}
+
+void Character::addAction(Action * act)
+{
+    actions.push_back(act);
 }
 
 void Character::setLife(double life)
